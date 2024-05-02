fe64a592d53234e1c1681a429dfe250ecefe7167
aroslavgoncar <yaroslav.honchar.1@gmail.com>
2024-05-02 21:02:42 +0300

    feat: configured and connected to mongoz

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,23 @@ import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ReviewModule } from './review/review.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
-    imports: [ConfigModule.forRoot(), AuthModule, ProductModule, TopPageModule, ReviewModule],
+    imports: [
+        ConfigModule.forRoot(),
+        TypegooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: getMongoConfig,
+        }),
+        AuthModule,
+        ProductModule,
+        TopPageModule,
+        ReviewModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })

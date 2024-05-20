a6905aa37c2f984579fd027a3adae126193691ce
Yaroslav Honchar <yaroslav.honchar.1@gmail.com>
2024-05-20 11:50:13 +0300

    feat: serve static files

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,6 +3,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { FileElementResponse } from './dto/file-element.response';
 import { FilesService } from './files.service';
+import { MFile } from './mfile.class';
 
 @Controller('files')
 export class FilesController {
@@ -12,7 +13,20 @@ export class FilesController {
     @HttpCode(200)
     @UseGuards(JwtAuthGuard)
     @UseInterceptors(FileInterceptor('files'))
-    uploadFile(@UploadedFile('file') file: Express.Multer.File): Promise<FileElementResponse[]> {
-        return this.filesService.saveFiles([file]);
+    async uploadFile(@UploadedFile('file') file: Express.Multer.File): Promise<FileElementResponse[]> {
+        const saveArr: MFile[] = [new MFile(file)];
+
+        if (file.mimetype.includes('image')) {
+            const webPBuffer = await this.filesService.convertToWebp(file.buffer);
+
+            saveArr.push(
+                new MFile({
+                    originalname: `${file.originalname.split('.')[0]}.webp`,
+                    buffer: webPBuffer,
+                }),
+            );
+        }
+
+        return this.filesService.saveFiles(saveArr);
     }
 }
